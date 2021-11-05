8fb32262bf0a7c78e89b4ea9ebbbf9a2d4d51bed
Neo <32793@ma-web.nl>
2021-11-05 09:59:51 +0100

    Ik ben cool

---
+++ TellMeMore/TellMeMore.py
@@ -0,0 +1,17 @@
+import time
+
+print("Hoe oud ben jij?")
+Leeftijd = input()
+time.sleep(1)
+
+print("Heb je broers en/of zussen?")
+Familie = input()
+time.sleep(1)
+
+print("Hoe vindt je het hier op school?")
+Mening = input()
+time.sleep(1)
+
+print(Leeftijd)
+print(Familie)
+print(Mening)
