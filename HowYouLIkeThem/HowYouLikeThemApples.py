ac4662a15a5ec03d286d82180ce4b683c9a38c3f
Neo <32793@ma-web.nl>
2021-11-05 09:59:02 +0100

    Ik ben cool

---
+++ HowYouLIkeThem/HowYouLikeThemApples.py
@@ -0,0 +1,8 @@
+SDProduction = 146 / 100 * 80
+SDBomen = 333 / 3 * 2
+AlleAppelsSD = SDBomen * SDProduction
+Bomen = 333 / 3 * 1
+BomenProductie = 146
+AlleAppelsBomen = Bomen * BomenProductie
+AlleAppels = AlleAppelsBomen + AlleAppelsSD
+print(AlleAppels)
